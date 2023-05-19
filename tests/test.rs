b86cd9550627c2fa73c0488facd8ec0a40be478a
Mado <nanikamado@gmail.com>
2023-05-19 17:36:41 +0900

    Add tests

---
+++ tests/test.rs
@@ -0,0 +1,44 @@
+use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+use std::process::Command;
+
+fn test_examples(file_name: &str, stdout: &str) {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg(["examples/", file_name].concat())
+        .assert()
+        .stdout(stdout.to_string())
+        .success();
+}
+
+fn test_test_fail(file_name: &str) {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg(["tests/fail/", file_name].concat())
+        .assert()
+        .code(1);
+}
+
+#[test]
+fn bin_tree() {
+    test_examples("bin_tree.ofl", "ok\n");
+}
+
+#[test]
+fn red_black_tree() {
+    test_examples("red_black_tree.ofl", "ok\n");
+}
+
+#[test]
+fn helloworld() {
+    test_examples("helloworld.ofl", "Hello, world.\n");
+}
+
+#[test]
+fn r#match() {
+    test_examples("match.ofl", "True\n");
+}
+
+#[test]
+fn fail_inexhaustive_match() {
+    test_test_fail("inexhaustive_match.ofl");
+}
