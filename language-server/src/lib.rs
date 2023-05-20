9d8db3ded5e69fb646938423b2cd9ed83d7c35e6
Mado <nanikamado@gmail.com>
2023-05-20 19:07:50 +0900

    Add language server

---
+++ language-server/src/lib.rs
@@ -0,0 +1,282 @@
+use dashmap::DashMap;
+use itertools::Itertools;
+use std::fs;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+type HoverMap = Vec<Vec<Option<Hover>>>;
+
+#[derive(Debug, PartialEq, Eq)]
+struct TokenCache {
+    hover_map: HoverMap,
+    state: TokenCacheState,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum TokenCacheState {
+    Fresh,
+    Unsaved,
+}
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    tokens: DashMap<Url, TokenCache>,
+}
+
+const SUPPORTED_TYPES: &[SemanticTokenType] = &[
+    SemanticTokenType::STRING,
+    SemanticTokenType::NUMBER,
+    SemanticTokenType::TYPE,
+    SemanticTokenType::new("constructor"),
+    SemanticTokenType::INTERFACE,
+    SemanticTokenType::FUNCTION,
+    SemanticTokenType::VARIABLE,
+    SemanticTokenType::KEYWORD,
+    SemanticTokenType::COMMENT,
+    SemanticTokenType::OPERATOR,
+    SemanticTokenType::new("constructorOperator"),
+];
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        self.client
+            .log_message(MessageType::INFO, "initializing")
+            .await;
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::INCREMENTAL,
+                )),
+                semantic_tokens_provider: Some(
+                    SemanticTokensOptions {
+                        legend: SemanticTokensLegend {
+                            token_types: SUPPORTED_TYPES.to_vec(),
+                            token_modifiers: Vec::new(),
+                        },
+                        full: Some(SemanticTokensFullOptions::Bool(true)),
+                        range: None,
+                        ..Default::default()
+                    }
+                    .into(),
+                ),
+                hover_provider: Some(HoverProviderCapability::Simple(true)),
+                ..ServerCapabilities::default()
+            },
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "initialized")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+        self.client
+            .log_message(MessageType::INFO, "configuration changed")
+            .await;
+    }
+
+    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+        self.client
+            .log_message(MessageType::INFO, "watched files changed")
+            .await;
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(
+                MessageType::INFO,
+                &format!("opened {}.", params.text_document.uri),
+            )
+            .await;
+        self.compile_uri(params.text_document.uri, Some(params.text_document.text))
+            .await;
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        self.client
+            .log_message(
+                MessageType::INFO,
+                &format!("changed {}", params.text_document.uri),
+            )
+            .await;
+        if let Some(mut t) = self.tokens.get_mut(&params.text_document.uri) {
+            t.state = TokenCacheState::Unsaved;
+        }
+    }
+
+    async fn did_save(&self, params: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file saved.")
+            .await;
+        let uri = params.text_document.uri;
+        if let Some(r) = self.tokens.get_mut(&uri) {
+            self.client
+                .log_message(MessageType::INFO, "document found")
+                .await;
+            let is_fresh = r.value().state == TokenCacheState::Fresh;
+            if is_fresh {
+                self.client
+                    .log_message(MessageType::INFO, "already cached.")
+                    .await;
+                return;
+            }
+        } else {
+            self.client
+                .log_message(MessageType::INFO, "not cached yet.")
+                .await;
+        }
+        self.compile_uri(uri, params.text).await;
+    }
+
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file closed.")
+            .await;
+    }
+
+    async fn semantic_tokens_full(
+        &self,
+        _params: SemanticTokensParams,
+    ) -> tower_lsp::jsonrpc::Result<Option<SemanticTokensResult>> {
+        self.client
+            .log_message(MessageType::INFO, "semantic tokens requested.")
+            .await;
+        Ok(None)
+    }
+
+    async fn hover(&self, params: HoverParams) -> tower_lsp::jsonrpc::Result<Option<Hover>> {
+        let position = params.text_document_position_params.position;
+        let url = params.text_document_position_params.text_document.uri;
+        if let Some(hover_map) = self.tokens.get(&url) {
+            Ok(hover_map
+                .value()
+                .hover_map
+                .get(position.line as usize)
+                .and_then(|t| t.get(position.character as usize).cloned()?))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Backend {
+    async fn compile_uri(&self, uri: Url, text: Option<String>) {
+        self.client
+            .log_message(MessageType::INFO, "compiling.")
+            .await;
+        if let Some(src) = text.or_else(|| fs::read_to_string(uri.path()).ok()) {
+            if let Ok(Some(hover_map)) =
+                tokio::task::spawn_blocking(move || semantic_token_and_hover_map(&src)).await
+            {
+                self.tokens.insert(
+                    uri,
+                    TokenCache {
+                        hover_map,
+                        state: TokenCacheState::Fresh,
+                    },
+                );
+                self.client
+                    .log_message(MessageType::INFO, "successfully compiled.")
+                    .await;
+            } else {
+                self.client
+                    .log_message(MessageType::INFO, format!("could not compile {uri}."))
+                    .await;
+            }
+        } else {
+            self.client
+                .log_message(MessageType::INFO, format!("{uri} not found."))
+                .await;
+        }
+    }
+}
+
+#[tokio::main]
+pub async fn run() {
+    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        tokens: Default::default(),
+    });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+fn semantic_token_and_hover_map(src: &str) -> Option<HoverMap> {
+    let (char_to_utf16_map, utf16_to_char_map) = make_map(src);
+    let mut ts_tail = compiler::token_map(src, "filename")?
+        .into_iter()
+        .sorted_by_key(|(s, _)| s.start);
+    let mut ts_head = ts_tail.next().unwrap();
+    let utf16_to_token_map = utf16_to_char_map
+        .into_iter()
+        .map(|utf16_to_char_line| {
+            utf16_to_char_line
+                .into_iter()
+                .map(|char| {
+                    char.and_then(|char| {
+                        while ts_head.0.end <= char {
+                            ts_head = ts_tail.next()?;
+                        }
+                        if ts_head.0.contains(char) {
+                            Some(Hover {
+                                contents: HoverContents::Markup(MarkupContent {
+                                    value: format!("```\n{}\n```", ts_head.1),
+                                    kind: MarkupKind::Markdown,
+                                }),
+                                range: Some(Range {
+                                    start: Position {
+                                        line: char_to_utf16_map[ts_head.0.start].0,
+                                        character: char_to_utf16_map[ts_head.0.start].1,
+                                    },
+                                    end: Position {
+                                        line: char_to_utf16_map[ts_head.0.end].0,
+                                        character: char_to_utf16_map[ts_head.0.end].1,
+                                    },
+                                }),
+                            })
+                        } else {
+                            None
+                        }
+                    })
+                })
+                .collect()
+        })
+        .collect();
+    Some(utf16_to_token_map)
+}
+
+type Utf16ToCharMap = Vec<Vec<Option<usize>>>;
+
+fn make_map(src: &str) -> (Vec<(u32, u32)>, Utf16ToCharMap) {
+    let mut char_to_utf16_map = Vec::with_capacity(src.len());
+    let mut utf16_to_char_map = Vec::new();
+    let mut utf16_to_char_line = Vec::new();
+    char_to_utf16_map.push((0, 0));
+    let mut line = 0;
+    let mut col_utf16 = 0;
+    for (char_i, c) in src.chars().enumerate() {
+        if c == '\n' {
+            utf16_to_char_map.push(utf16_to_char_line);
+            utf16_to_char_line = Vec::new();
+            line += 1;
+            col_utf16 = 0;
+        } else {
+            utf16_to_char_line.resize(col_utf16 as usize, None);
+            col_utf16 += c.len_utf16() as u32;
+        }
+        utf16_to_char_line.push(Some(char_i));
+        char_to_utf16_map.push((line, col_utf16));
+    }
+    utf16_to_char_map.push(utf16_to_char_line);
+    (char_to_utf16_map, utf16_to_char_map)
+}
--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,34 @@ use std::process::exit;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
+#[clap(arg_required_else_help(true))]
 struct Args {
-    /// File name
-    file: String,
-    #[arg(short('c'), long)]
+    #[arg(required_unless_present_any(["language_server"]))]
+    file: Option<String>,
     /// Output generated c code
+    #[arg(short('c'), long)]
     emit_c: bool,
+    /// start language server
+    #[arg(short('l'), long, exclusive(true))]
+    language_server: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    let s = fs::read_to_string(&args.file).unwrap();
-    if args.emit_c {
-        compile(&s, &args.file, &mut stdout());
-    } else if let Ok(exit_status) = run_c::run(|w| compile(&s, &args.file, w)) {
-        exit(exit_status.code().unwrap());
+    if args.language_server {
+        #[cfg(feature = "language-server")]
+        language_server::run();
+        #[cfg(not(feature = "language-server"))]
+        panic!();
     } else {
-        exit(1);
+        let file_name = args.file.unwrap();
+        let s = fs::read_to_string(&file_name).unwrap();
+        if args.emit_c {
+            compile(&s, &file_name, &mut stdout());
+        } else if let Ok(exit_status) = run_c::run(|w| compile(&s, &file_name, w)) {
+            exit(exit_status.code().unwrap());
+        } else {
+            exit(1);
+        }
     }
 }
