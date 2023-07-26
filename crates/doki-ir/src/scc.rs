55f5bb195f441bdfc64fa8f5cef4c9f6cd94a9a3
Mado <nanikamado@gmail.com>
2023-07-27 01:15:21 +0900

    Fix `scc`

--- crates/doki-ir/src/scc.rs
+++ crates/doki-ir/src/scc.rs
@@ -17,13 +17,14 @@ fn pre_sort<T: PartialOrd + Eq + std::hash::Hash + Copy>(
 
 fn collect_scc<T: PartialOrd + Eq + std::hash::Hash + Copy>(
     v: T,
-    rg: &mut FxHashMap<T, FxHashSet<T>>,
+    rg: &FxHashMap<T, FxHashSet<T>>,
     c: &mut Vec<T>,
+    to_be_done: &mut FxHashSet<T>,
 ) {
-    if let Some(ws) = rg.remove(&v) {
-        c.push(v);
-        for w in ws {
-            collect_scc(w, rg, c);
+    c.push(v);
+    for w in &rg[&v] {
+        if to_be_done.remove(w) {
+            collect_scc(*w, rg, c, to_be_done);
         }
     }
 }
@@ -31,15 +32,16 @@ fn collect_scc<T: PartialOrd + Eq + std::hash::Hash + Copy>(
 pub fn scc<T: PartialOrd + Eq + std::hash::Hash + Copy>(
     entry_point: T,
     g: &FxHashMap<T, FxHashSet<T>>,
-    mut rg: FxHashMap<T, FxHashSet<T>>,
+    rg: FxHashMap<T, FxHashSet<T>>,
 ) -> Vec<Vec<T>> {
     let mut sorted = Vec::new();
-    pre_sort(entry_point, g, &mut sorted, &mut FxHashSet::default());
+    let mut reachable_nodes = FxHashSet::default();
+    pre_sort(entry_point, g, &mut sorted, &mut reachable_nodes);
     let mut cs = Vec::new();
     for v in sorted.into_iter().rev() {
-        if rg.contains_key(&v) {
+        if reachable_nodes.remove(&v) {
             let mut c = Vec::new();
-            collect_scc(v, &mut rg, &mut c);
+            collect_scc(v, &rg, &mut c, &mut reachable_nodes);
             cs.push(c);
         }
     }
@@ -85,6 +87,31 @@ mod tests {
             })
             .collect_vec();
         let correct: &[&[_]] = &[&[0], &[1], &[2, 3, 8], &[4], &[5, 6, 7]];
+        let correct = correct
+            .iter()
+            .map(|vs| vs.iter().copied().collect_vec())
+            .collect_vec();
+        assert_eq!(scc, correct);
+    }
+
+    #[test]
+    fn scc_2() {
+        let mut g: FxHashMap<u32, FxHashSet<u32>> = FxHashMap::default();
+        let mut rg: FxHashMap<u32, FxHashSet<u32>> = FxHashMap::default();
+        for (v, w) in &[(0, 1), (2, 1), (3, 2)] {
+            g.entry(*v).or_default().insert(*w);
+            g.entry(*w).or_default();
+            rg.entry(*w).or_default().insert(*v);
+        }
+        rg.entry(0).or_default();
+        let scc = scc(0, &g, rg)
+            .into_iter()
+            .map(|mut vs| {
+                vs.sort_unstable();
+                vs
+            })
+            .collect_vec();
+        let correct: &[&[_]] = &[&[0], &[1]];
         let correct = correct
             .iter()
             .map(|vs| vs.iter().copied().collect_vec())
