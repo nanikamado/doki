1f994e075d90aa6d23766ffb8137eeb87aeea713
Mado <nanikamado@gmail.com>
2023-08-29 15:14:38 +0900

    Remove unused includes

--- crates/doki-ir/src/codegen.rs
+++ crates/doki-ir/src/codegen.rs
@@ -70,8 +70,6 @@ impl Display for Codegen<'_> {
             "
             #include <stdio.h>
             #include <stdlib.h>
-            #include <string.h>
-            #include <stdint.h>
             #include <inttypes.h>
             #include <unistd.h>
             struct diverge{{}};
